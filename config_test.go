20c89fd6d610eb242ed86d16b46dad9f7824d412
Fabian Freund <github@fabi.online>
2025-09-30 13:55:25 +0200

    update test to use ParseConfigFromText

--- config_test.go
+++ config_test.go
@@ -2,19 +2,7 @@ package wiresocks
 
 import (
 	"testing"
-
-	"github.com/go-ini/ini"
 )
-
-func loadIniConfig(config string) (*ini.File, error) {
-	iniOpt := ini.LoadOptions{
-		Insensitive:            true,
-		AllowShadows:           true,
-		AllowNonUniqueSections: true,
-	}
-
-	return ini.LoadSources(iniOpt, []byte(config))
-}
 
 func TestWireguardConfWithoutSubnet(t *testing.T) {
 	const config = `
@@ -28,12 +16,8 @@ PublicKey = dGhpcyBpcyBhIHRlc3QgcHVibGljIGtleS4uLi4uLi4=
 AllowedIPs = 0.0.0.0/0
 Endpoint = 1.2.3.4:51820
 PersistentKeepalive = 25`
-	iniData, err := loadIniConfig(config)
-	if err != nil {
-		t.Fatal(err)
-	}
 
-	_, err = ParseInterface(iniData)
+	_, err := ParseConfigFromText(config)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -51,12 +35,8 @@ PublicKey = onemorekeyonemorekeyonemorekeyonemorekeynow=
 AllowedIPs = 192.168.1.0/24
 Endpoint = 5.6.7.8:51820
 PersistentKeepalive = 15`
-	iniData, err := loadIniConfig(config)
-	if err != nil {
-		t.Fatal(err)
-	}
 
-	_, err = ParseInterface(iniData)
+	_, err := ParseConfigFromText(config)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -73,12 +53,8 @@ DNS = 208.67.222.222,208.67.220.220
 PublicKey = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB="
 AllowedIPs = 0.0.0.0/0,::/0
 Endpoint = 9.9.9.9:51820`
-	iniData, err := loadIniConfig(config)
-	if err != nil {
-		t.Fatal(err)
-	}
 
-	_, err = ParseInterface(iniData)
+	_, err := ParseConfigFromText(config)
 	if err != nil {
 		t.Fatal(err)
 	}
